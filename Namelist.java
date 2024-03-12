2b483c59da1726442fb549f72d9121ff18054685
Sawol Tesfaghebriel <sawoltesf@gmail.com>
2024-03-12 18:34:32 -0500

    Refactor MySearchTree class to use generics and add new methods for adding and finding nodes

--- Namelist.java
+++ Namelist.java
@@ -1,7 +1,39 @@
-class MySearchTree<T>{
+class MySearchTree<T extends Comparable<T>>{
 
   Node root;
-  
+
+  MySearchTree(){
+    root = null;
+  }
+
+  public Node add(T value, Node root){
+    if(root == null){
+      return new Node(value, null, null);
+    }
+
+    if(root.data.compareTo(value) < 0){
+      root.right = add(value, root.right);
+    }
+    else if(root.data.compareTo(value) > 0){
+      root.left = add(value, root.left);
+    }
+
+    return root;
+  }
+  public boolean find(T value, Node root){
+    if(root == null){
+      return false;
+    }
+    if(root.data.compareTo(value) < 0){
+      return find(value, root.right);
+    }
+    else if(root.data.compareTo(value) > 0){
+      return find(value, root.left);
+    }
+    else{
+      return true;
+    }
+  }
   
   private class Node{
     T data;
