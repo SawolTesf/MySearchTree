4f45bb583b312d306b8b5632094d0b628b66a473
Sawol Tesfaghebriel <sawoltesf@gmail.com>
2024-03-12 17:52:04 -0500

    Refactor MySearchTree class and add Node inner class

--- Namelist.java
+++ Namelist.java
@@ -1,9 +1,29 @@
-class MySearchTree{
+class MySearchTree<T>{
+
+  Node root;
   
+  
+  private class Node{
+    T data;
+    Node left;
+    Node right;
+
+    Node(){
+      data = null;
+      right = null;
+      left = null;
+    }
+    Node(T data, Node left, Node right){
+      this.data = data;
+      this.left = left;
+      this.right = right;
+    }
+
+  }
 }
 
 class NameList{
-  public static void NameList(){
+  public static void main(){
 
   }
 }
